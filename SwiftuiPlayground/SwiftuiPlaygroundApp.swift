a4421f826c77deaa711fa72e162e2232ff910ee7
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2023-03-05 14:33:29 +0100

    Initial Commit

---
+++ SwiftuiPlayground/SwiftuiPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiPlaygroundApp.swift
+//  SwiftuiPlayground
+//
+//  Created by Kaori Persson on 2023-03-05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
